5ebdb42cd62208d5a905d629c0a0551e1d609a7b
Artem Egorov <Artem-Egorov13@mail.ru>
2022-10-05 21:26:26 +0300

    Create programm

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,21 @@
 public class Main {
+    public static void main(String[] args) {
+        int replenishment = 2000;
+        int clientCash = 3200;
+        int cashing = replenishment + clientCash;
+        int bonus = 100;
+        int clientBonus = cashing / bonus;
+        int plusBonus = replenishment / 100;
+        int finish = clientBonus+ plusBonus;
+        if (replenishment < 1000) {
+            System.out.println("Текущий счет: " + cashing);
+            System.out.println("Начисленный бонус: 0");
+            System.out.println("Итоговый бонус: " + clientBonus);
+
+        } else {
+            System.out.println("Текущий счет: " + cashing);
+            System.out.println("Начисленный бонус: " + plusBonus);
+            System.out.println("Итоговый бонус: " + finish);
+          }
+}
 }
